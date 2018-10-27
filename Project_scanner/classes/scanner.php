4aca70ed32d8606096ec44a520416506b918affa
ELWAHAB <andruha.unhurian@gmail.com>
2018-10-27 19:55:41 +0300

    some 3  for scanner, but don't works

--- Project_scanner/classes/scanner.php
+++ Project_scanner/classes/scanner.php
@@ -6,11 +6,19 @@
  * Time: 01:41
  */
 
+//$direct = "../FTP";
 //$scan = new Scanner();
 
-//$scan->scanning("../FTP");
-//var_dump($scan->get_list_files());
 
+//$scan->record_dir($direct);
+//var_dump($scan->getListDir($direct));
+//$scan->directInf($direct);
+
+
+//var_dump($scan->getListDir($direct));
+//$scan->scanning($direct);
+//$scan->scanning($direct);
+//var_dump($scan->get_list_files());
 
 
 class Scanner{
@@ -18,15 +26,100 @@ class Scanner{
     private $direct;
     public $index = 0;
 
+    public $listAllDir;
+
     function __construct($direct = "../FTP" ){
         $this->direct = $direct;
     }
 
     // нові функції через ітератори для запису масив даних про файли
 
+    // ------------- 3 -й варіант роботи з ітераторами -------------------------
+
+    public function scanning($direct){
+        $this->record_dir($direct); // функція записала в масив $listAllDir усі існуючі директорії
+
+        // пройдемось по масиву $listAllDir циклом foreach
+        foreach ($this->listAllDir as $patch){
+            if ($patch != Null ){
+                $this->directInf( (string) $patch); // передаємо шлях до кожної нової директорії для запису інформації
+            }
+        }
+    }
+
+    // функція для запису в масив усі існуючі директорії
+    public function record_dir($direct){
+        $this->listAllDir[] = $direct;
+
+        $recursDir = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($direct,
+            RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::SELF_FIRST,
+            RecursiveIteratorIterator::CATCH_GET_CHILD);
+
+        foreach ($recursDir as $item){
+            if ($item->isDir()){
+                $path = str_replace("\\","/",$item);
+                $this->listAllDir[] = $path;
+            }
+
+        }
+    }
+
+
+    //функція для запису в масив інформації по змісту переданої шляху $path до директорії
+    public function directInf($path){
+        $dir = new DirectoryIterator($path);
+
+                foreach ($dir as $directoryIterator){
+            if (!$directoryIterator->isDot()){
+                $str = str_replace("\\", "/",$directoryIterator->getPathname() );
+
+                if ($directoryIterator->isDir()){
+                    $this->file_details($this->index, $str, $directoryIterator);
+                    $this->list_file[$this->index]['type'] = 0;
+                    $this->index++;
+                }else{
+                    $this->file_details($this->index, $str, $directoryIterator);
+                    $this->list_file[$this->index]['type'] = 1;
+                    $this->index++;
+                }
+            }
+        }
+    }
+
+    //функція повертає масив із існуючими директоріями в даній директорії $direct
+    public function getListDir($direct){
+        $this->record_dir($direct);
+        return $this->listAllDir;
+    }
+
+    // повертає масив із списком інформації усіх файлів/директорій
+    public function getFileInfo($direct){
+        $this->directInf($direct);
+        return $this->list_file;
+    }
+
+
+    // функція записую всю інформацію по даному файлі/папці
+    public function file_details($index = 0, $patch, DirectoryIterator $directoryIterator){
+
+        if(!$directoryIterator->isDir()) {
+            $this->list_file[$index]['hash']= md5(file_get_contents($patch));
+        } else {
+            $this->list_file[$index]['hash']= md5($patch);
+        }
+        $this->list_file[$index]['patch']= $patch;
+        $this->list_file[$index]['last_edit'] = $directoryIterator->getMTime();
+        $this->list_file[$index]['size']= $directoryIterator->getSize();
+        $this->list_file[$index]['last_scan']= time();
+    }
+
+
+
     // ------------- 2-й варіант роботи з ітераторами ---------------------------
     // ----- Працює ------
-   /* public function scanning($direct){
+
+/*    public function scanning($direct){
         $dir = new DirectoryIterator($direct);
 
         foreach ($dir as $key => $directoryIterator){
@@ -134,7 +227,7 @@ class Scanner{
 
     // старі функції через сканування директорії для запису масив даних про файли
     //---------------------------------------------------------------------------
-    public function scanning( $direct ){
+    /*public function scanning( $direct ){
         $scan = scandir($direct);
              foreach ($scan as $item){
                 if ( ($item != ".") & ($item != "..")){
@@ -163,7 +256,7 @@ class Scanner{
         $this->list_file[$index]['last_edit'] = filemtime($patch) ;
         $this->list_file[$index]['size']= filesize($patch);
         $this->list_file[$index]['last_scan']= time();
-    }
+    }*/
 
 
 
